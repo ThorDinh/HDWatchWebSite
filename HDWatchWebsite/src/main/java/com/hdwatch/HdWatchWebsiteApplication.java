9428519303f110e72a2b54d0e55c80d845f0d29f
HienBuiT <kurokirigaming@gmail.com>
2023-05-31 20:25:33 +0700

    Upload Project
    - Create Project
    - Create packages
    - Add templates

---
+++ HDWatchWebsite/src/main/java/com/hdwatch/HdWatchWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.hdwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdWatchWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdWatchWebsiteApplication.class, args);
+	}
+
+}
---
+++ HDWatchWebsite/src/test/java/com/hdwatch/HdWatchWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdWatchWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
