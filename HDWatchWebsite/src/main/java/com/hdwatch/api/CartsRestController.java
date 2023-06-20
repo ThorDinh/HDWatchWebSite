38edd172a02781aa6b702364a90e4deb37939544
HienBuiT <kurokirigaming@gmail.com>
2023-06-20 09:36:12 +0700

    merge RestAPI into Develop

--- HDWatchWebsite/src/main/java/com/hdwatch/api/CartsRestController.java
+++ HDWatchWebsite/src/main/java/com/hdwatch/api/CartsRestController.java
@@ -2,8 +2,6 @@ package com.hdwatch.api;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,28 +20,28 @@ import com.hdwatch.service.CartsService;
 @RestController
 @RequestMapping("/rest/carts")
 public class CartsRestController {
-	@Autowired
-	CartsService cartsService;
-	
-	@GetMapping
-	public List<Carts> getAll() {
-		return cartsService.findAll();
-	}
-	
-	@GetMapping("{id}")
-	public Carts getOne(@PathVariable("id")Integer id) {
-		return cartsService.findById(id);
-	}
-	@PostMapping
-	public Carts createCarts(@RequestBody Carts carts) {
-		return cartsService.create(carts);
-	}
-	@PutMapping("{id}")
-	public Carts updateCarts(@PathVariable("id")Integer id,@RequestBody Carts carts) {
-		 return cartsService.save(carts, id);
-	}
-	@DeleteMapping("{id}")
-	public void deleteCart(@PathVariable("id")Integer id) {
-		cartsService.deleteById(id);
-	}
+//	@Autowired
+//	CartsService cartsService;
+//	
+//	@GetMapping
+//	public List<Carts> getAll() {
+//		return cartsService.findAll();
+//	}
+//	
+//	@GetMapping("{id}")
+//	public Carts getOne(@PathVariable("id")Integer id) {
+//		return cartsService.findById(id);
+//	}
+//	@PostMapping
+//	public Carts createCarts(@RequestBody Carts carts) {
+//		return cartsService.create(carts);
+//	}
+//	@PutMapping("{id}")
+//	public Carts updateCarts(@PathVariable("id")Integer id,@RequestBody Carts carts) {
+//		 return cartsService.save(carts, id);
+//	}
+//	@DeleteMapping("{id}")
+//	public void deleteCart(@PathVariable("id")Integer id) {
+//		cartsService.deleteById(id);
+//	}
 }
