bb3f6d816a204955b3c42db29525fd277c909871
Nguyễn Khả Định <124996164+ThorDinh@users.noreply.github.com>
2023-06-20 21:15:25 +0700

    Update RestFul API
    Accounts, Roles, Productimages,

---
+++ HDWatchWebsite/src/main/java/com/hdwatch/service/ProductimagesService.java
@@ -0,0 +1,18 @@
+package com.hdwatch.service;
+
+import java.util.List;
+
+import com.hdwatch.entity.Productimages;
+
+public interface ProductimagesService {
+	
+//	List<Productimages> findAll();
+//	
+//	Productimages findById(Integer id);
+//	
+//	Productimages create(Productimages productimages);
+//	
+//	Productimages save(Integer id,Productimages productimages);
+//	
+//	void deleteById(Integer id);
+}
---
+++ HDWatchWebsite/src/main/java/com/hdwatch/service/RolesService.java
@@ -0,0 +1,18 @@
+package com.hdwatch.service;
+
+import java.util.List;
+
+import com.hdwatch.entity.Roles;
+
+public interface RolesService {
+//	
+//	List<Roles> findAll();
+//	
+//	Roles findById(Integer id);
+//	
+//	Roles create(Roles roles);
+//	
+//	Roles save(Integer id, Roles roles);
+//	
+//	void deleteById(Integer id);
+}
