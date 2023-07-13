adc2d71817fc57dd367d1906f7c36b7f146489b6
HienBuiT <kurokirigaming@gmail.com>
2023-07-13 11:04:01 +0700

    up code
    - fix front end

--- HDWatchWebsite/src/main/java/com/hdwatch/controller/CartController.java
+++ HDWatchWebsite/src/main/java/com/hdwatch/controller/CartController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
-	@RequestMapping("/cart-view")
+	@RequestMapping("/cart/view")
 	public String cart(Model model) {
 		model.addAttribute("pageTitle", "Giỏ hàng");
 		return "cart/view";
