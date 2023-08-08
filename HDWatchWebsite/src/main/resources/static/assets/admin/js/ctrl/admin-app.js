8d36dd327662adad9adfe3b576210a44b4ab5f90
HienBuiT <kurokirigaming@gmail.com>
2023-08-08 22:20:14 +0700

    fix project structure

--- HDWatchWebsite/src/main/resources/static/assets/admin/js/ctrl/admin-app.js
+++ HDWatchWebsite/src/main/resources/static/assets/admin/js/ctrl/admin-app.js
@@ -4,27 +4,27 @@ const app = angular.module("admin-app",["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider
         .when("/category",{
-            templateUrl:"/admin/assets/layout/category.html",
+            templateUrl:"/assets/admin/layout/category.html",
             controller:"category-ctrl"
         })
         .when("/brand",{
-            templateUrl:"/admin/assets/layout/brand.html",
+            templateUrl:"/assets/admin/layout/brand.html",
             controller:"brand-ctrl"
         })
         .when("/product",{
-            templateUrl:"/admin/assets/layout/product.html",
+            templateUrl:"/assets/admin/layout/product.html",
             controller:"product-ctrl"
         })
         .when("/user",{
-            templateUrl:"/admin/assets/layout/user.html",
+            templateUrl:"/assets/admin/layout/user.html",
             controller:"user-ctrl"
         })
         .when("/authenticate",{
-            templateUrl:"/admin/assets/layout/authenticate.html",
+            templateUrl:"/assets/admin/layout/authenticate.html",
             controller:"authenticate-ctrl"
         })
         .otherwise({
-            templateUrl:"/admin/assets/layout/dashboard.html",
+            templateUrl:"/assets/admin/layout/dashboard.html",
             controller:"dashboard-ctrl"
         })
 });
